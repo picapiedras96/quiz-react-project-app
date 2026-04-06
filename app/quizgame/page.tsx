2235b6f7ab7ed343b48ada0d8ecb02d4b7ab60fc
Pedro Ibarra <ibarra.pedro96@icloud.com>
2026-04-06 13:49:31 -0400

    added custom light daisy-ui theme, updated tailwindCSS default colors and added basick start game page

--- app/quizgame/page.tsx
+++ app/quizgame/page.tsx
@@ -1,10 +1,15 @@
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-6">
-      <h1 className="text-4xl font-bold">{"Pedro's Quiz Game"}</h1>
-      <button className="btn btn-primary">Start Quiz</button>
-      <button className="btn btn-secondary">View Scores</button>
-      <button className="btn btn-accent btn-outline">Settings</button>
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-3xl font-bold font-heading">Quizzical</h1>
+          <p className="py-6 text-base">
+            A fun little quiz game built with Next.js, Tailwind CSS and DaisyUI.
+          </p>
+          <button className="btn btn-primary btn-md px-12 py-6 text-base rounded-2xl">Start Quiz</button>
+        </div>
+      </div>
     </div>
   );
 }
